abb0b4ba26ceb94e6f9e51808ed24be25b3d8bc1
Ricardo Fabian de Mello Albuquerque Junior <ricardofabian141@gmail.com>
2024-01-03 13:48:35 -0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,8 @@
-import Tabela from './components/Tabela/index.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bolsa de Valores do Brasil (B3)</h1>
-      <Tabela />
     </div>
   );
 }
